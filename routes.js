b91b1af0e62064746e20c81e0468b8f3e7c0a7b9
thomas-delplace <thomasrenedelplace@gmail.com>
2025-06-04 06:27:17 +0200

    first commit

---
+++ routes.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const routes = express.Router()
+
+////////////////////////////////////////////////// POST
+
+routes.post('/necklace', (req,res)=>{
+    const transporter = nodemailer.createTransport({
+        host:process.env.SENDER_HOST,
+        port:process.env.SENDER_PORT,
+        secure:false,
+        requireTLS:process.env.SENDER_TLS,
+        auth:{
+            user:process.env.SENDER_EMAIL,
+            pass:process.env.SENDER_PASS
+        }
+    })
+    const mailOptions = {
+        from:process.env.SENDER_EMAIL,
+        to:process.env.RECEIVER_EMAIL,
+        subject:'Commande de collier personnalisé',
+        text:`Tu viens de recevoir une demande de ${req.body.client.mail} (${req.body.client.firstName} ${req.body.client.lastName}) pour un collier personnalisé. \n\n Chaine ${req.body.necklace.chain}, \n ${req.body.necklace.beads}, \n message : ${req.body.necklace.message}`
+    }
+    transporter.sendMail(mailOptions, (error,info)=>{
+        if(error){
+            res.status(500).json({message:"Oops, une erreur s'est produite. Veuillez réessayer."})
+        }else{
+            res.status(200).json({message:"La commande a bien été transmise."})
+        }
+    })
+})
+
+routes.post('/on',(req,res)=>{
+    res.status(200).json({message:"okay"})
+})
+module.exports = routes
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const dotenv = require('dotenv')
+const path = require('path')
+
+
+const envPath = path.resolve('.env')
+dotenv.config({path:envPath})
+
+const API_URL = process.env.API_URL
+const API_PORT = process.env.API_PORT
+const APP_URL = process.env.APP_URL
+
+app.use(cors({
+    origin: APP_URL,
+    methods:'POST',
+    credentials:true
+}))
+app.use(express.json())
+
+const routes = require('./routes.js')
+app.use('/', routes)
+
+app.listen(API_PORT, ()=>{
+    console.log(`Serveur en cours d\'execution à l'adresse : ${API_URL}:${API_PORT}`)
+})
